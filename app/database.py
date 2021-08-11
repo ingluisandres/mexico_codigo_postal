7e51b2b138aa33557043eb5c46bac29a055cc0c1
ingluisandres <ingluisandres3@gmail.com>
2021-08-11 10:07:53 -0600

    Fixed the location of the database in database.py

--- app/database.py
+++ app/database.py
@@ -2,7 +2,7 @@ import sqlalchemy as _sql
 import sqlalchemy.ext.declarative as _declarative
 import sqlalchemy.orm as _orm
 
-SQLALCHEMY_DATABASE_URL = 'sqlite:///./database.db'
+SQLALCHEMY_DATABASE_URL = 'sqlite:///./app/database.db'
 
 engine = _sql.create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread':False}
