b5504777c1e269c3d25a6e80c3e9aaffe37a719a
ingluisandres <ingluisandres3@gmail.com>
2021-08-21 11:47:21 -0600

    mysql container works with uvicorn

--- app/database.py
+++ app/database.py
@@ -2,10 +2,12 @@ import sqlalchemy as _sql
 import sqlalchemy.ext.declarative as _declarative
 import sqlalchemy.orm as _orm
 
-SQLALCHEMY_DATABASE_URL = 'mysql+pymysql://root:somepassword@prueba-db-contanedor:3306/codigo_postal'
+SQLALCHEMY_DATABASE_URL = 'mysql+pymysql://root:somepassword@localhost:3306/codigo_postal'
+
+
 
 engine = _sql.create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread':False}
+    SQLALCHEMY_DATABASE_URL, pool_pre_ping=True
 )
 
 SessionLocal = _orm.sessionmaker(autocommit=False, autoflush=False, bind=engine)
