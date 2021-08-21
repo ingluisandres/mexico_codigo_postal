c613fa3b8762275659b86f6b80ea45f15fa721a8
ingluisandres <ingluisandres3@gmail.com>
2021-08-21 17:33:41 -0600

    Added sqlite database for development

--- app/config/database.py
+++ app/config/database.py
@@ -2,13 +2,17 @@ import sqlalchemy as _sql
 import sqlalchemy.ext.declarative as _declarative
 import sqlalchemy.orm as _orm
 
-SQLALCHEMY_DATABASE_URL = 'mysql+pymysql://root:somepassword@contenedor-sql:3306/codigo_postal'
 
+# SQLALCHEMY_DATABASE_URL = 'mysql+pymysql://root:somepassword@contenedor-sql:3306/codigo_postal'
+# engine = _sql.create_engine(
+#     SQLALCHEMY_DATABASE_URL, pool_pre_ping=True
+# )
 
-
+SQLALCHEMY_DATABASE_URL = 'sqlite:///./app/config/database.db'
 engine = _sql.create_engine(
-    SQLALCHEMY_DATABASE_URL, pool_pre_ping=True
+    SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread':False}
 )
+
 
 SessionLocal = _orm.sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
