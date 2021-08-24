0dc2fb7c751e923df48483e8ad6c06ca4e4cf4b3
ingluisandres <ingluisandres3@gmail.com>
2021-08-23 20:00:50 -0600

    Updated services.py

--- app/services/services.py
+++ app/services/services.py
@@ -15,7 +15,7 @@ from app.services import services
 SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
 ALGORITHM = "HS256"
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="v1/login")
 
 
 def get_admin(db: Session=Depends(database.get_db), token: str = Depends(oauth2_scheme)):
